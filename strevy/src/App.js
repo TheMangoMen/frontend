850d79e2a9b3c3fea3cf78c62326b93acc5c4114
sumedhdhan <sumedh.dhan@gmail.com>
2024-05-25 20:33:19 -0400

    Bitcoin metrics test API call

--- strevy/src/App.js
+++ strevy/src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(()=> {
+    fetch('https://api.coincap.io/v2/assets')
+      .then(response => response.json())
+      .then(json =>setData(json))
+      .catch(error=>console.error(error));
+  },
+  []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Personal finances tracker
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : 'Loading...'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
