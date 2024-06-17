7150f4cabf3e5148e64d937fc1c16529c96a090d
Norman Chen <njchen@uwaterloo.ca>
2024-06-16 20:17:41 -0400

    make status work for interview and ranking stages

--- watrank/src/components/ui/stepper.tsx
+++ watrank/src/components/ui/stepper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Bot, ContactRoundIcon, DollarSign, Minus } from "lucide-react"; // Import specific icons
+import { Bot, ContactRoundIcon, DollarSign, Minus, ListOrdered, Ban, Trophy } from "lucide-react"; // Import specific icons
 import { Status } from "@/app/jobs/status";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip";
 
@@ -8,7 +8,7 @@ interface StepperProps {
 }
 
 const Stepper: React.FC<StepperProps> = ({ steps }) => {
-	const currentStep = steps.length - 1; // Always set to the last step
+	const currentStep = steps ? steps.length - 1 : 0; // Always set to the last step
 
 	// Define fixed icons for the first and last steps
 	const preAssessmentIcon = Bot;
@@ -16,18 +16,30 @@ const Stepper: React.FC<StepperProps> = ({ steps }) => {
 
 	// Define an icon to use for the middle steps
 	const interviewIcon = ContactRoundIcon;
+
+	const takingIcon = Trophy;
+	const notTakingIcon = Ban;
+	const rankedIcon = ListOrdered;
+
+	const iconMap = {
+		"OA": preAssessmentIcon,
+		"Offer Call": offerCallIcon,
+		"Interview": interviewIcon,
+		"Ranked": rankedIcon,
+		"Taking": takingIcon,
+		"Not Taking": notTakingIcon,
+	}
     
 	return (
 		<div>
 			<div className="flex">
-				{steps.map((step, index) => {
+				{steps?.map((step, index) => {
 					let IconComponent;
-					if (step.name === "OA") {
-						IconComponent = preAssessmentIcon;
-					} else if (step.name === "Offer") {
-						IconComponent = offerCallIcon;
-					} else {
-						IconComponent = interviewIcon;
+					if (step.name in iconMap) {
+						IconComponent = iconMap[step.name]
+					}
+					else {
+						IconComponent = iconMap["Interview"]
 					}
 
 					return (
@@ -36,20 +48,25 @@ const Stepper: React.FC<StepperProps> = ({ steps }) => {
 								<Tooltip>
 									<TooltipTrigger>
 										<div className="flex items-center">
+											{index > 0 && (
+												<div className="flex-auto">
+													&ndash;
+												</div>
+											)}
 											<div
 												className={`flex items-center justify-center w-8 h-8 rounded-full ${
 													index === currentStep ? "bg-primary/80 " : "bg-transparent outline outline-primary/80"
 												} ${index < currentStep ? "opacity-75" : ""}`}
 											>
 												<IconComponent className={`w-4 h-4 ${index === currentStep ? "text-black" : ""}`} strokeWidth={2} />{" "}
-												{/* Render the icon */}
 											</div>
 											{index < steps.length - 1 && (
 												<div className="flex-auto">
-													&mdash;
+													&ndash;
 												</div>
 											)}
 										</div>
+										{["Ranked", "Taking", "Not Taking"].includes(step.name) && <p>{step.count}</p>}
 									</TooltipTrigger>
 									<TooltipContent>
 										<div className="text-sm">{step.name}: {step.count}</div>
