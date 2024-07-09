23a21fb390845cf5491724c33f260c79b02e4c0f
Norman Chen <njchen@uwaterloo.ca>
2024-07-08 22:00:34 -0400

    Add logs

--- watrank/src/app/admin/page.tsx
+++ watrank/src/app/admin/page.tsx
@@ -21,39 +21,44 @@ import {
 } from "@/components/ui/select"
 import { useAuth } from "@/context/AuthContext";
 import { useToast } from "@/components/ui/use-toast";
+import { DataTable } from "@/components/ui/data-table"
+import { ColumnDef } from "@tanstack/react-table"
 
 const ADMIN_CARD_DATA = [
     {
-        title: "Update Stage",
+        title: "Stage",
         description: "Update the current stage",
         placeholder: "Enter stage",
         buttonText: "Update Stage",
         fetchUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/stage`,
         updateUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/stage`,
-        inputType: "boolean",
+        inputType: "dropdown",
         dataKey: "isRankingStage",
         toastTitle: "Update Successful",
         toastDescription: "Ranking stage updated",
-        dropdownOptions: undefined,
+        dropdownOptions: [
+            { label: "Ranking Stage", value: true },
+            { label: "Interview Stage", value: false },
+        ],
     },
     {
-        title: "Update Year",
-        description: "Update the current year",
-        placeholder: "Enter year",
-        buttonText: "Update Year",
-        fetchUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/year`,
-        updateUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/year`,
+        title: "Cycle",
+        description: "Update the current cycle",
+        placeholder: "Enter cycle",
+        buttonText: "Update Cycle",
+        fetchUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/cycle`,
+        updateUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/cycle`,
         inputType: "number",
-        dataKey: "year",
+        dataKey: "cycle",
         toastTitle: "Update Successful",
-        toastDescription: "Year updated",
+        toastDescription: "Cycle updated",
         dropdownOptions: undefined,
     },
     {
-        title: "Update Season",
-        description: "Update the current season",
-        placeholder: "Enter season",
-        buttonText: "Update Season",
+        title: "Term",
+        description: "Update the current term",
+        placeholder: "Enter term",
+        buttonText: "Update Term",
         fetchUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/season`,
         updateUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/season`,
         inputType: "dropdown",
@@ -66,17 +71,18 @@ const ADMIN_CARD_DATA = [
             { label: "Winter", value: "Winter" },
         ],
     },
+    // TODO: remove current year and just get year from POSTGRES
     {
-        title: "Update Cycle",
-        description: "Update the current cycle",
-        placeholder: "Enter cycle",
-        buttonText: "Update Cycle",
-        fetchUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/cycle`,
-        updateUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/cycle`,
+        title: "Year",
+        description: "Update the current year",
+        placeholder: "Enter year",
+        buttonText: "Update Year",
+        fetchUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/year`,
+        updateUrl: `${process.env.NEXT_PUBLIC_API_URL}/admin/year`,
         inputType: "number",
-        dataKey: "cycle",
+        dataKey: "year",
         toastTitle: "Update Successful",
-        toastDescription: "Cycle updated",
+        toastDescription: "Year updated",
         dropdownOptions: undefined,
     },
 ];
@@ -93,7 +99,7 @@ interface AdminCardProps {
     token: string;
     toastTitle: string;
     toastDescription: string;
-    dropdownOptions?: { label: string; value: string | number }[];
+    dropdownOptions?: { label: string; value: string | number | boolean }[];
 }
 
 const AdminCard: React.FC<AdminCardProps> = ({
@@ -172,19 +178,19 @@ const AdminCard: React.FC<AdminCardProps> = ({
     };
 
     return (
-        <Card className="m-4">
+        <Card className="m-4 w-64">
             <CardHeader>
                 <CardTitle>{title}</CardTitle>
                 <CardDescription>{description}</CardDescription>
             </CardHeader>
-            <CardContent>
-                <form onSubmit={handleSubmit}>
+            <CardContent >
+                <form onSubmit={handleSubmit} className="w-full">
                     {inputType === 'dropdown' && dropdownOptions ? (
-                        <Select value={value} onValueChange={handleSelectChange}>
-                            <SelectTrigger className="w-[180px]">
+                        <Select value={value} onValueChange={handleSelectChange} >
+                            <SelectTrigger className="w-full">
                                 <SelectValue placeholder={placeholder} />
                             </SelectTrigger>
-                            <SelectContent>
+                            <SelectContent className="w-full">
                                 {dropdownOptions?.map((option, index) => (
                                     <SelectItem key={index} value={String(option.value)}>
                                         {option.label}
@@ -193,7 +199,7 @@ const AdminCard: React.FC<AdminCardProps> = ({
                             </SelectContent>
                         </Select>
                     ) : inputType === 'boolean' ? (
-                        <Select value={value} onValueChange={handleSelectChange}>
+                        <Select value={value} onValueChange={handleSelectChange} >
                             <SelectTrigger className="w-[180px]">
                                 <SelectValue placeholder={placeholder} />
                             </SelectTrigger>
@@ -208,6 +214,7 @@ const AdminCard: React.FC<AdminCardProps> = ({
                             value={value}
                             onChange={handleChange}
                             placeholder={placeholder}
+                            className="w-full"
                         />
                     )}
                     <Button className="w-full mt-4" type="submit">{buttonText}</Button>
@@ -227,6 +234,37 @@ interface ContributionLog {
     OfferCall: boolean;
 }
 
+const columns: ColumnDef<ContributionLog>[] = [
+    {
+        accessorKey: "LogID",
+        header: "Log ID"
+    },
+    {
+        accessorKey: "LogTime",
+        header: "Log Time"
+    },
+    {
+        accessorKey: "UID",
+        header: "User ID"
+    },
+    {
+        accessorKey: "JID",
+        header: "Job ID"
+    },
+    {
+        accessorKey: "OA",
+        header: "OA"
+    },
+    {
+        accessorKey: "InterviewStage",
+        header: "Interview Stage"
+    },
+    {
+        accessorKey: "OfferCall",
+        header: "Offer Call"
+    },
+]
+
 export default function AdminPage() {
     const { token, authIsLoading } = useAuth()
     const [contributionLogs, setContributionLogs] = useState<ContributionLog[]>([])
@@ -238,7 +276,19 @@ export default function AdminPage() {
                 headers: { ...(!!token && { 'Authorization': `Bearer ${token}` }) }
             });
             const data = await response.json();
-            setContributionLogs(data);
+            const formattedData = data.map((entry: ContributionLog) => {
+                const date = new Date(entry.LogTime);
+                const formattedLogTime = `${date.toLocaleDateString('en-US')} ${date.toLocaleTimeString('en-US', {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit'
+                })}`;
+                return {
+                    ...entry,
+                    LogTime: formattedLogTime
+                };
+            });
+            setContributionLogs(formattedData);
         } catch (error) {
             console.error('Error fetching initial value:', error);
         }
@@ -250,45 +300,47 @@ export default function AdminPage() {
         }
     }, [authIsLoading])
 
-    useEffect(() => {
-        console.log(contributionLogs)
-    }, [contributionLogs])
-
-
 
     if (authIsLoading) {
         return <div>Loading...</div>;
     }
     return (
-        <>
-            <div className="px-10 flex flex-wrap">
-                {ADMIN_CARD_DATA.map((card, index) => (
-                    <AdminCard
-                        key={index}
-                        title={card.title}
-                        description={card.description}
-                        placeholder={card.placeholder}
-                        buttonText={card.buttonText}
-                        fetchUrl={card.fetchUrl}
-                        updateUrl={card.updateUrl}
-                        inputType={card.inputType as any}
-                        dataKey={card.dataKey}
-                        token={token || ""}
-                        toastTitle={card.toastTitle}
-                        toastDescription={card.toastDescription}
-                        dropdownOptions={card.dropdownOptions}
-                    />
-                ))}
-            </div>
-            <div>
-                {/* {contributionLogs.map((log) => ( */}
-                {/*     <div key={log.LogID}> */}
-                {/*         {log.LogID}, {log.LogTime}, {log.UID}, {log.JID}, {log.OA}, {log.InterviewStage}, {log.OfferCall} */}
-                {/*     </div> */}
-                {/* ))} */}
-            </div>
-
-        </>
+        <div className="px-10 flex flex-col gap-4">
+            <Card className="">
+                <CardHeader>
+                    <CardTitle>Status</CardTitle>
+                    <CardDescription>Update current statuses of the platform</CardDescription>
+                </CardHeader>
+                <CardContent className="justify-center items-center flex flex-wrap">
+                    {ADMIN_CARD_DATA.map((card, index) => (
+                        <AdminCard
+                            key={index}
+                            title={card.title}
+                            description={card.description}
+                            placeholder={card.placeholder}
+                            buttonText={card.buttonText}
+                            fetchUrl={card.fetchUrl}
+                            updateUrl={card.updateUrl}
+                            inputType={card.inputType as any}
+                            dataKey={card.dataKey}
+                            token={token || ""}
+                            toastTitle={card.toastTitle}
+                            toastDescription={card.toastDescription}
+                            dropdownOptions={card.dropdownOptions}
+                        />
+                    ))}
+                </CardContent>
+            </Card>
+            <Card className="">
+                <CardHeader>
+                    <CardTitle>Logs</CardTitle>
+                    <CardDescription>View logs for recent contributions</CardDescription>
+                </CardHeader>
+                <CardContent className="">
+                    <DataTable columns={columns} data={contributionLogs} />
+                </CardContent>
+            </Card>
+        </div>
     )
 }
 
