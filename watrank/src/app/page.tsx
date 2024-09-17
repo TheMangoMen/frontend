9154074af1d2cfcea0003a122ca48a34c6d23b2f
abhinav-bala <abhinav.bala24@gmail.com>
2024-09-17 00:27:30 -0400

    removed carousel

--- watrank/src/app/page.tsx
+++ watrank/src/app/page.tsx
@@ -1,33 +1,29 @@
 "use client"
 import Image from "next/image"
-import Autoplay from "embla-carousel-autoplay"
 import React, { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
-import { ArrowLeft, ArrowRight } from "lucide-react";
 import Link from 'next/link';
-import { motion, AnimatePresence } from 'framer-motion';
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 const slides = [
   {
-    title: "1. Import all your applied jobs from WaterlooWorks",
+    title: "1. Import applied jobs from WaterlooWorks",
     description: "Share and access anonymous information about co-op job searches to make informed decisions.",
     image: "/landing/import-jobs.png"
   },
   {
-    title: "2. Real-time application progress",
+    title: "2. Track the status of postings in real-time",
     description: "Track and view the progress of job applications, from initial submission to final interviews.",
     image: "/landing/check-status.png"
   },
   {
-    title: "3. Anonymously share your own status",
+    title: "3. Share your updates anonymously",
     description: "Update/Add to the status of jobs you have applied to.",
     image: "/landing/contribute.png"
   },
   {
-    title: "4. Observe and track trends",
+    title: "4. Discover trends and insights",
     description: "Gain insights on your co-op application journey.",
     image: "/landing/analytics.png"
   }
@@ -39,19 +35,8 @@ const LandingPage = () => {
       <div className="text-4xl font-bold text-center text-foreground">
         Navigate your next co-op search with confidence.
       </div>
-      <div className="px-20">
-      <Carousel className="w-full"   opts={{
-        align: "start",
-        loop: true,
-      }}
-      plugins={[
-        Autoplay({
-          delay: 4000,
-        }),
-      ]}>
-        <CarouselContent>
+      <div className="px-20 grid md:grid-cols-2 gap-5">
           {slides.map((slide, index) => (
-            <CarouselItem key={index}>
               <Card className="p-4">
                 <CardTitle className="mb-2 font-bold text-xl">{slide.title}</CardTitle>
                 <CardContent className="flex items-center justify-center p-2">
@@ -62,12 +47,8 @@ const LandingPage = () => {
                 </div>     
                 </CardContent>
               </Card>
-            </CarouselItem>
           ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+
       </div>
       
 
