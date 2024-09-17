16b3d7dc9db4847a187595f290231d57cb22fd7f
abhinav-bala <abhinav.bala24@gmail.com>
2024-09-17 18:23:35 -0400

    made landing page ui friendly
    Co-authored-by: Sumedh Dhanvanthry <sumedhdhan@users.noreply.github.com>

--- watrank/src/app/page.tsx
+++ watrank/src/app/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import Link from 'next/link';
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { motion } from 'framer-motion';
 
 const slides = [
   {
@@ -31,34 +32,58 @@ const slides = [
 
 const LandingPage = () => {
   return (
-    <div className="max-h-screen flex flex-col items-center justify-between pt-4 gap-5">
-      <div className="text-4xl font-bold text-center text-foreground">
+      <motion.div 
+      className="max-h-screen flex flex-col items-center justify-between pt-4 gap-5"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <motion.div 
+        className="text-4xl font-bold text-center text-foreground text-balance"
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.2, duration: 0.5 }}
+      >
         Navigate your next co-op search with confidence.
-      </div>
-      <div className="px-20 grid md:grid-cols-2 gap-5">
-          {slides.map((slide, index) => (
-              <Card key={index} className="p-4">
-                <CardTitle className="mb-2 font-bold text-xl">{slide.title}</CardTitle>
-                <CardContent className="flex items-center justify-center p-2">
+      </motion.div>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{ delay: 0.4, duration: 0.5 }}
+      >
+        <Link href="/jobs" passHref>
+          <Button className="font-md rounded-md text-lg">
+            Start Searching
+          </Button>
+        </Link>
+      </motion.div>
+      <motion.div 
+        className="px-4 md:px-20 grid md:grid-cols-2 gap-5"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.6, duration: 0.5 }}
+      >
+        {slides.map((slide, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.8 + index * 0.1, duration: 0.5 }}
+          >
+            <Card className="p-4">
+              <CardTitle className="mb-2 font-bold text-xl">{slide.title}</CardTitle>
+              <CardContent className="flex items-center justify-center p-2">
                 <div className="w-full">
                   <AspectRatio ratio={16 / 9}>
                     <Image src={slide.image} fill={true} alt={slide.description} className="rounded-md object-cover" />
                   </AspectRatio>
-                </div>     
-                </CardContent>
-              </Card>
-          ))}
-
-      </div>
-      
-
-
-        <Link href="/jobs" passHref>
-          <Button className="font-bold rounded-md text-lg">
-            Start Searching
-          </Button>
-        </Link>
-    </div>
+                </div>
+              </CardContent>
+            </Card>
+          </motion.div>
+        ))}
+      </motion.div>
+    </motion.div>
   );
 };
 
