4719e99711a8651cb3adc891181a226e53593102
Michael Xu <61663933+xuchef@users.noreply.github.com>
2024-10-03 13:40:04 -0600

    center on mobile

--- watrank/src/app/page.tsx
+++ watrank/src/app/page.tsx
@@ -96,7 +96,7 @@ const LandingPage = () => {
 				className="relative py-12 lg:py-24 max-w-6xl px-4 lg:px-8 h-screen grid lg:grid-cols-2 mx-auto content-center lg:items-center justify-center gap-y-8 -mb-6"
 			>
 				<div className="mb-8 lg:mb-0">
-					<div>
+					<div className="flex justify-center lg:justify-start">
 						<div className="bg-secondary/10 p-6 rounded-full inline-block mb-6 shadow-banner border border-muted animate-bounce">
 							<span className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 dark:from-blue-400 dark:to-pink-400 font-bold">
 								Rankings out now!
