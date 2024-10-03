e7416d7fd590eb06d6f81a61480d3b4cf673d649
Michael Xu <61663933+xuchef@users.noreply.github.com>
2024-10-03 13:37:33 -0600

    animations

--- watrank/src/app/page.tsx
+++ watrank/src/app/page.tsx
@@ -97,7 +97,7 @@ const LandingPage = () => {
 			>
 				<div className="mb-8 lg:mb-0">
 					<div>
-						<div className="bg-secondary/10 p-6 rounded-full inline-block mb-6 shadow-banner border border-muted">
+						<div className="bg-secondary/10 p-6 rounded-full inline-block mb-6 shadow-banner border border-muted animate-bounce">
 							<span className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 dark:from-blue-400 dark:to-pink-400 font-bold">
 								Rankings out now!
 							</span>
@@ -121,7 +121,8 @@ const LandingPage = () => {
 
 				<div className="lg:w-full bg-black text-white p-4 rounded border border-gold font-mono flex flex-col shadow-stats">
 					<h2 className="text-2xl flex items-center">
-						<span className="w-3 h-3 bg-red-500 rounded-full ml-2 mr-6 shadow-red"></span>
+						<span className="w-5 h-5 bg-red-500 rounded-full ml-2 mr-6 opacity-55 absolute inline-flex animate-ping shadow-red"></span>
+						<span className="w-3 h-3 bg-red-500 rounded-full ml-3 mr-6 shadow-red"></span>
 						Live Stats
 					</h2>
 
