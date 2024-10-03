2d5e9bd6aa4e2b2bef04bade0d9533cbd0998c6b
Michael Xu <61663933+xuchef@users.noreply.github.com>
2024-10-03 14:27:45 -0600

    not huge on mobile

--- watrank/src/app/page.tsx
+++ watrank/src/app/page.tsx
@@ -98,7 +98,7 @@ const LandingPage = () => {
 				<div className="mb-8 lg:mb-0">
 					<div className="flex justify-center lg:justify-start">
 						<div className="bg-secondary/10 p-6 rounded-full inline-block mb-6 shadow-banner border border-muted animate-bounce">
-							<span className="text-3xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 dark:from-blue-400 dark:to-pink-400 font-bold">
+							<span className="text-xl lg:text-3xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 dark:from-blue-400 dark:to-pink-400 font-bold">
 								Rankings out now!
 							</span>
 						</div>
