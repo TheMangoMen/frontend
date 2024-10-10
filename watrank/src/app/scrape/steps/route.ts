650d08212e946d3d924e32f5ffd4e406e122453f
HusseinElguindi <34726859+HusseinElguindi@users.noreply.github.com>
2024-10-09 22:06:48 -0400

    add notion scrape steps route

---
+++ watrank/src/app/scrape/steps/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    return NextResponse.redirect(
+        "https://glib-hornet-03b.notion.site/Scraping-Jobs-With-UW-Wave-11bf1713f28b80deb33ee961e1e26c02?pvs=4",
+    );
+}
