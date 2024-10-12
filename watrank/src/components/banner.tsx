8a5d4c587515136652d6e354ca55cda13372bb40
Michael Xu <61663933+xuchef@users.noreply.github.com>
2024-10-11 19:41:05 -0600

    yolo cycle 2

--- watrank/src/components/banner.tsx
+++ watrank/src/components/banner.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Banner: React.FC = () => {
     return (
-        <div className="bg-secondary/10 p-6 rounded-full inline-block mb-6 shadow-banner border border-muted">
+        <div className="bg-secondary/10 p-6 rounded-full inline-block mb-6 shadow-banner border border-muted animate-bounce">
             <span className="text-xl lg:text-3xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 dark:from-blue-400 dark:to-pink-400 font-bold">
-                Cycle 1 done. Go outside!
+                Cycle 2 jobs are up!
             </span>
         </div>
     );
